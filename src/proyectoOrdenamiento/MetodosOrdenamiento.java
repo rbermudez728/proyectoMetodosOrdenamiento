3b09a23c44c33e5f2da43463c5862fd96b6d7431
rbermudez728 <tusk@utp.edu.co>
2016-02-24 22:55:36 -0500

    update

---
+++ src/proyectoOrdenamiento/MetodosOrdenamiento.java
@@ -0,0 +1,20 @@
+package proyectoOrdenamiento;
+
+import java.io.IOException;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class MetodosOrdenamiento {
+	public List<Integer> seleccion(List<Integer> datos){
+		return datos;
+	}
+	public List<Integer> burbuja(List<Integer> datos){
+		return datos;
+	}
+	public List<Integer> ordenamientoRapido(List<Integer> datos){
+		return datos;
+	}
+	public List<Integer> conteo(List<Integer> datos){
+		return datos;
+	}
+}
---
+++ src/proyectoOrdenamiento/Principal.java
@@ -0,0 +1,24 @@
+package proyectoOrdenamiento;
+
+import java.io.IOException;
+
+public class Principal {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			LectorDeDatos ld = new LectorDeDatos(1l);
+			ld.generarDatos();
+			ld.leerDatos();
+			System.out.println(ld.datos.size());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		MetodosOrdenamiento mo = new MetodosOrdenamiento();
+	}
+
+}
