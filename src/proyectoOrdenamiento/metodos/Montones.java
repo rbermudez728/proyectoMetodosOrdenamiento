5f17aea958b9a8c157b8e06c5fa7bc2c940c1430
rbermudez728 <tusk@utp.edu.co>
2016-03-02 23:09:43 -0500

    rapido y montones funcionando

--- src/proyectoOrdenamiento/metodos/Montones.java
+++ src/proyectoOrdenamiento/metodos/Montones.java
@@ -1,6 +1,8 @@
 package proyectoOrdenamiento.metodos;
 
 import java.util.List;
+import java.util.PriorityQueue;
+
 import proyectoOrdenamiento.utiles.Heap; // Importando la estructura de datos montículo para su uso en el algoritmo
 
 public class Montones extends Thread{
@@ -12,7 +14,21 @@ public class Montones extends Thread{
 
 	public void run() {
 		// Aquí va el codigo del algoritmo
-			
+		 this.heapSort();
 		System.out.println("Termina el algoritmo de ordenamiento por Montones con un tiempo de: ");
 	}
+	
+	private void heapSort(){
+		 PriorityQueue<Integer> heap = new PriorityQueue<Integer>(datos.size());
+		    
+		    // Add each array element to the heap
+		    for (Integer e : datos)
+		        heap.add(e);
+		    
+		    // Elements come off the heap in ascending order
+		    for (int i=0; i<datos.size(); i++)
+		    	datos.set(i,heap.remove());
+	}
+	
+	
 }
--- src/proyectoOrdenamiento/metodos/Rapido.java
+++ src/proyectoOrdenamiento/metodos/Rapido.java
@@ -9,50 +9,50 @@ public class Rapido extends Thread {
 	public Rapido(List<Integer> muestra) {
 		this.datos = muestra;
 
-		int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
-		int len = 9;
-
-		System.out.println();
-		System.out.println("QuickSort By Iterative Method");
-		QuickSort(numbers, 0, len - 1);
-		for (int i = 0; i < 9; i++)
-			System.out.println(numbers[i]);
 
 	}
 
 	public void run() {
 		// Aquí va el codigo del algoritmo
 
+		/*int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+		int len = 9;*/
+
+	//	System.out.println();
+//		System.out.println("QuickSort By Iterative Method");
+		this.quickSort(0, this.datos.size() - 1);
+/*		for (int i = 0; i < 9; i++)
+			System.out.println(numbers[i]);*/
 		System.out.println("Termina el algoritmo de ordenamiento por Ordenamiento Rápido con un tiempo de: ");
 	}
 
-	public static int Partition(int[] numbers, int left, int right) {
-		int pivot = numbers[left];
+	public int partition(int left, int right) {
+		int pivot = this.datos.get(left);
 		while (true) {
-			while (numbers[left] < pivot)
+			while (this.datos.get(left) < pivot)
 				left++;
 
-			while (numbers[right] > pivot)
+			while (this.datos.get(right)> pivot)
 				right--;
 
 			if (left < right) {
-				int temp = numbers[right];
-				numbers[right] = numbers[left];
-				numbers[left] = temp;
+				int temp = this.datos.get(right);
+				this.datos.set(right,this.datos.get(left));
+				this.datos.set(left,temp);
 			} else {
 				return right;
 			}
 		}
 	}
 
-	public static class QuickPosInfo {
+	public class QuickPosInfo {
 		public int left;
 		public int right;
 	};
 
-	public static QuickPosInfo info = new QuickPosInfo();
+	public QuickPosInfo info = new QuickPosInfo();
 
-	public static void QuickSort(int[] numbers, int left, int right) {
+	public void quickSort(int left, int right) {
 
 		if (left >= right)
 			return; // Invalid index range
@@ -71,7 +71,7 @@ public class Rapido extends Thread {
 			right = list.get(0).right;
 			list.remove(0);
 
-			int pivot = Partition(numbers, left, right);
+			int pivot = partition(left, right);
 
 			if (pivot > 1) {
 				info.left = left;
